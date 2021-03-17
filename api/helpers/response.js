052139cec555eaae7fbc551ff739df2a57c6ae4a
Tarsis de Lima <br.tmvdl@gmail.com>
2021-03-16 22:27:06 -0300

    Fixing response errors

--- api/helpers/response.js
+++ api/helpers/response.js
@@ -1,16 +1,22 @@
 
 const ok = (res) => {
+  const status = 'success'
+  const message = null
   res.ok = (data = null) =>
-    res.status(200).json({ data, message: null, status: 'success' })
+    res.status(200).json({ data, message, status })
 }
 
-const nok = (res) => {
-  res.nok = (status, message = 'Error', data = null) =>
-    res.status(status).json({ data, message, status: 'error' })
+const error = (res) => {
+  const status = 'status'
+  res.error = (error) => {
+    console.error({ error })
+    const { status: statusCode = 500, message = 'Server error', data = null } = error
+    res.status(statusCode).json({ data, message, status })
+  }
 }
 
 module.exports = (_, res, next) => {
   ok(res)
-  nok(res)
+  error(res)
   next()
 }
