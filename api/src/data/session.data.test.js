64e5c54aad66700cb4fd5315595ec4d01f383848
Tarsis de Lima <br.tmvdl@gmail.com>
2021-03-19 20:19:23 -0300

    Adding tests of data layer

---
+++ api/src/data/session.data.test.js
@@ -0,0 +1,13 @@
+// const { createSession, getSession } = require('./session.data')
+
+describe('Tests for session creation', () => {
+  it.todo('Should create a session')
+  it.todo('Should not create a session with blank params')
+  it.todo('Should not create a session without permissions')
+})
+
+describe('Tests for session getting', () => {
+  it.todo('Should get a session')
+  it.todo('Should not get a session with blank key')
+  it.todo('Should not get a session without params')
+})
