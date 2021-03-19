a37f0d5ef9070240503dbbc5b2158c27b16c02e8
Tarsis de Lima <br.tmvdl@gmail.com>
2021-03-19 20:30:50 -0300

    Adding tests of data layer

---
+++ api/src/data/users.data.test.js
@@ -0,0 +1,54 @@
+// const { createUser, getUserById, getUserByUsername, getUserByUsernameAndPassword, getUserWithPassword, getUser, deleteUser, editUser, listUsers } = require('./users.data')
+
+describe('Tests for creation of users', () => {
+  it.todo('Should create a new user')
+  it.todo('Should not create a new user with blank username')
+  it.todo('Should not create a new user with blank password')
+  it.todo('Should not create a new user with blank permissions')
+  it.todo('Should not create a new user with blank params')
+  it.todo('Should not create a new user with no params')
+})
+
+describe('Tests for listting users', () => {
+  it.todo('Should return a list of users')
+  it.todo('Should not return a list of users')
+})
+
+describe('Tests for getting of users by id', () => {
+  it.todo('Should get a user by id')
+  it.todo('Should get a user with blank id')
+  it.todo('Should get a user with no id')
+})
+
+describe('Tests for getting of users by username', () => {
+  it.todo('Should get a user by username')
+  it.todo('Should get a user with blank username')
+  it.todo('Should get a user with no username')
+})
+
+describe('Tests for getting of users by username and password', () => {
+  it.todo('Should get a user by username and password')
+  it.todo('Should get a user with blank username and password')
+  it.todo('Should get a user with no username and password')
+})
+
+describe('Tests for getting of users with all fields', () => {
+  it.todo('Should get a user with all fields')
+  it.todo('Should get a user with no params')
+})
+
+describe('Tests for getting of users', () => {
+  it.todo('Should get a user by filter')
+  it.todo('Should not get a user with no filter')
+  it.todo('Should not get a user with no params')
+})
+
+describe('Tests for editting users', () => {
+  it.todo('Should edit a user')
+  it.todo('Should not edit a user')
+})
+
+describe('Tests for deleting users', () => {
+  it.todo('Should delete a user')
+  it.todo('Should not delete a user')
+})
