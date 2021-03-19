1e5cdd5f6150c2593d0b64b22753b312e5d5dce3
Tarsis de Lima <br.tmvdl@gmail.com>
2021-03-19 20:34:14 -0300

    Adding tests of service layer

---
+++ api/src/services/session.service.test.js
@@ -0,0 +1,9 @@
+// const { createSession } = require('./session.service')
+
+describe('Tests for session creations', () => {
+  it.todo('Should create a new session')
+  it.todo('Should not create a new session with blank username')
+  it.todo('Should not create a new session with blank password')
+  it.todo('Should not create a new session with blank uername and password')
+  it.todo('Should not create a new session with no params')
+})
